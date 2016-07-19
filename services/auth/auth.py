8777df8c73a9eee9aefef76f59e94785a6fed2d4
Christian Grabowski <cpg1111@users.noreply.github.com>
2016-07-19 17:41:27 -0400

    add test configs (#5)
    * add test configs
    
    * travis.yml -> .travis.yml
    
    * .circle.yml -> circle.yml
    
    * fix circle service name
    
    * handle sudo in travis
    
    * fix sudo in circle
    
    * add encrypted data to travis
    
    * pyopenssl for circle

--- services/auth/auth.py
+++ services/auth/auth.py
@@ -5,14 +5,23 @@ from twisted.internet.defer import inlineCallbacks, returnValue
 
 app = Klein()
 
+class Auth:
+    def hello(request):
+      # hello = yield "hello"
+      # returnValue(hello)
+      return "hello"
+
+auth = Auth()
+
 @app.route("/")
 @inlineCallbacks
 def hello(request):
-    hello = yield "hello"
+    hello = yield auth.hello()
     returnValue(hello)
 
 # @app.route("/login", METHODS = ["POST"])
 # @inlineCallbacks
 # def login(request):
 
-app.run("0.0.0.0", 8080)
+if __name__ == "__main__":
+    app.run("0.0.0.0", 8080)
